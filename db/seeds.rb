7cac28fcb24d3fbc12918e646254904611061085
Amelie Boudier <amelie.boudier01@gmail.com>
2020-10-20 15:32:14 +0200

    created seeds for episodes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+require "nokogiri"
+
+Episode.destroy_all
+
+url = "https://www.accenture.com/de-de/careers/local/accenture-meets-esports?src=SOMS&c=car_glb_meetourpeople_11308187&n=smc_0920&abc=curated+daily+content_linkedin_100001570262257&linkId=100000016821004"
+
+html_doc = Nokogiri::HTML(open(url).read)
+
+puts "Scraping Accenture webpage..."
+
+html_doc.search('#block-section-profiles .modal-play').each do |element|
+  a = Episode.new
+
+  a.url = element.attribute('data-video-src').value
+  title = element.attribute('data-title').value
+  if title.match(/(.+)<.+>(.+)<\/a>(.+)<.+>(.+)<\/a>(.+)/)
+    a.title = title.match(/(.+)<.+>(.+)<\/a>(.+)<.+>(.+)<\/a>(.+)/)[1..5].join
+  else
+    a.title = title.match(/(.+)<.+>(.+)<\/a>(.+)/)[1..3].join
+  end
+  a.save!
+end
+
+puts "#{Episode.count} episodes were created!"
